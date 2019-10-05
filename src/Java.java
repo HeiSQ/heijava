237bd7f4f59a3c8460fb3879968703ef4ba2e7e0
heisq <1355377623@qq.com>
2019-10-05 12:24:14 +0800

    添加了three

--- src/Java.java
+++ src/Java.java
@@ -6,5 +6,6 @@ public class Java {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("hello two");
+        System.out.println("hello three");
     }
 }
